8007d286a030d382bbbe746a773293875379db43
葛云飞(拐爷) <yunfei.ge@17zuoye.com/geyunfei@gmail.com>
2021-01-31 23:34:19 +0800

    update server

--- fool-view/src/main/java/org/fool/framework/view/service/ViewDataService.java
+++ fool-view/src/main/java/org/fool/framework/view/service/ViewDataService.java
@@ -1,11 +1,13 @@
 package org.fool.framework.view.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.fool.framework.dao.DaoService;
 import org.fool.framework.view.model.View;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
+@Slf4j
 @Component
 public class ViewDataService {
 
@@ -13,6 +15,8 @@ public class ViewDataService {
     private DaoService daoService;
 
     public View getViewData(String viewName, String token) {
-        return daoService.getOneDetailByKey(View.class, viewName);
+        var view = daoService.getOneDetailByKey(View.class, viewName);
+        log.info("the view is :{}", view);
+        return view;
     }
 }
