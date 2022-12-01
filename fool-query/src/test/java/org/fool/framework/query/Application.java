a0372cee5acac1c780d0dfbc950f00e4c5b264b1
葛云飞(拐爷) <yunfei.ge@17zuoye.com/geyunfei@gmail.com>
2022-12-01 16:16:52 +0800

    feat: config

--- fool-query/src/test/java/org/fool/framework/query/Application.java
+++ fool-query/src/test/java/org/fool/framework/query/Application.java
@@ -1,13 +1,13 @@
-package org.fool.framework.query;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-@SpringBootApplication
-@ComponentScan(basePackages = {"org.fool.framework.common", "org.fool.framework.dao" , "org.fool.framework.query"})
-public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-}
+//package org.fool.framework.query;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.ComponentScan;
+//
+//@SpringBootApplication
+//@ComponentScan(basePackages = {"org.fool.framework.common", "org.fool.framework.dao" , "org.fool.framework.query"})
+//public class Application {
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+//}
--- fool-query/src/test/java/org/fool/framework/query/CompareFilterTest.java
+++ fool-query/src/test/java/org/fool/framework/query/CompareFilterTest.java
@@ -1,40 +1,40 @@
-package org.fool.framework.query;
-
-import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@Slf4j
-@RunWith(SpringRunner.class)
-@SpringBootApplication
-@SpringBootTest(classes = Application.class)
-public class CompareFilterTest {
-
-
-    @Test
-    public void revert() {
-        CompareFilter.LinkedNode<Integer> current = new CompareFilter.LinkedNode<Integer>();
-        CompareFilter.LinkedNode<Integer> head = current;
-        for (Integer i = 0; i < 10; i++) {
-            current.setData(i);
-            current.setNext(new CompareFilter.LinkedNode<Integer>());
-            current = current.getNext();
-        }
-        current.setNext(null);
-        var node = head;
-        while (node != null) {
-            log.info("current value :{}", node.getData());
-            node = node.getNext();
-        }
-        node = CompareFilter.revert(head);
-        log.info("the revert result");
-        while (node != null) {
-            log.info("current value :{}", node.getData());
-            node = node.getNext();
-        }
-
-    }
-}
+//package org.fool.framework.query;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@Slf4j
+////@RunWith(SpringRunner.class)
+//@SpringBootApplication
+////@SpringBootTest(classes = Application.class)
+//public class CompareFilterTest {
+//
+//
+//    @Test
+//    public void revert() {
+//        CompareFilter.LinkedNode<Integer> current = new CompareFilter.LinkedNode<Integer>();
+//        CompareFilter.LinkedNode<Integer> head = current;
+//        for (Integer i = 0; i < 10; i++) {
+//            current.setData(i);
+//            current.setNext(new CompareFilter.LinkedNode<Integer>());
+//            current = current.getNext();
+//        }
+//        current.setNext(null);
+//        var node = head;
+//        while (node != null) {
+//            log.info("current value :{}", node.getData());
+//            node = node.getNext();
+//        }
+//        node = CompareFilter.revert(head);
+//        log.info("the revert result");
+//        while (node != null) {
+//            log.info("current value :{}", node.getData());
+//            node = node.getNext();
+//        }
+//
+//    }
+//}
